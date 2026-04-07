96b4e70f835d44c7a0282a416af7c821adfc9a17
Bruno Silva De Oliveira OLIVEIRA <u09177187121@acad.pp.ifms.edu.br>
2026-04-07 15:19:00 -0400

    Criamos o bean e dao e a tabela de usuarios no bean.

---
+++ src/bean/MpvUsuarios.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bean;
+
+import java.util.Date;
+
+/**
+ *
+ * @author u09177187121
+ */
+public class MpvUsuarios {
+    int mpvIdUsuarios;
+    String mpvNome;
+    String mpvApelido;
+    String mpvCPF;
+    Date mpvDataNascimento;
+    int mpvNivel;
+    String mpvSenha;
+    String mpvAtivo;
+    
+}
